66ab3b6b7e8f52d0a53fbde833617511efc1e831
Unknown807 <milvovangveric@gmail.com>
2021-05-22 13:12:24 +0100

    Expanded mode and added more keywords

--- src/scripts/editor.js
+++ src/scripts/editor.js
@@ -1,35 +1,55 @@
 
-var keywords = ["press", "click"];
+let keywords = [
+    //Motor
+    "click", "hands","point", "type", "write",
+    //Cognitive
+    "confirm", "think", "read", "ignore", "remember", "store",
+    //Perceptual
+    "look", "hear", "say", "search",
+    //System Response
+    "response"
+];
 
-CodeMirror.defineMode("mymode", function() {
+let symbols = [
+    "(", ")", "mental-load",
+    "[", "]", "time-load",
+    "<", ">",  "task-amount",
+];
+
+CodeMirror.defineMode("mymode", () => {
 
   return {
 
-    startState: function() {return {inComment: false};},
-    token: function(stream, state) {
+    startState: () => {return {inComment: false, keywordUsed: false};},
+    token: (stream, state) => {
 
+        //One keyword can be highlighted per line
+        if (stream.sol()) {
+            state.keywordUsed = false;
+        }
+
+        // Checks for comments
         if (!state.inComment && stream.peek() == "*") {
             stream.next();
             state.inComment = true;
-        } else if (stream.peek() == "(") {
-            stream.next();
-            if (!state.inComment && stream.skipTo(")")) {
-                stream.next();
-                return "mental-load";
-            }
-        } else if (stream.peek() == "[") {
-            stream.next();
-            if (!state.inComment && stream.skipTo("]")) {
-                stream.next();
-                return "time-load"
-            }
-        } else if (stream.eatWhile(/\w/)) {
+        } else if (!state.keywordUsed && stream.eatWhile(/\w/)) {// Checks for keywords
             if (keywords.includes(stream.current().toLowerCase())) {
+                state.keywordUsed = true;
                 return "task";
+            }
+        } else { // Checks for use of symbols together
+            for (i=0; i<symbols.length; i+=3) {
+                if (stream.peek() == symbols[i]) {
+                    stream.next();
+                    if (!state.inComment && stream.skipTo(symbols[i+1])) {
+                        stream.next();
+                        return symbols[i+2];
+                    }
+                }
             }
         }
 
-        if (state.inComment) {
+        if (state.inComment) {// Multiline comments
             if (stream.skipTo("*")) {
                 stream.next();
                 state.inComment = false;
@@ -39,16 +59,14 @@ CodeMirror.defineMode("mymode", function() {
 
             return "comment";
         }
-        
+
         stream.next();
     }
-
   };
-
 });
 
-
 let editor = $("#editor")[0];
+
 let codemirror_editor = CodeMirror.fromTextArea(editor, {
     lineNumbers: true,
     mode: "mymode",
