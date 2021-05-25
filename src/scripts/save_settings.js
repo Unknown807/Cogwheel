2f335734dfbdbbfb45dbda5bb8e97d60d467dc04
Unknown807 <milvovangveric@gmail.com>
2021-05-25 14:45:14 +0100

    Settings can be saved via button on page, instead of when leaving page

--- src/scripts/save_settings.js
+++ src/scripts/save_settings.js
@@ -1,11 +1,25 @@
 
-$("#back-button").on("click", () => {
+function getSettings() {
     let settings = [];
     for (i=0; i<16; i++) {
         settings.push($(`#${inputs[i]}-input`).val());
     }
     settings.push(currentTheme);
 
+    return settings
+}
+
+$("#back-button").on("click", () => {
+    let settings = getSettings();
     window.api.send("saveSettings", settings);
     window.location.href = "index.html";
+});
+
+$("#save-button").on("click", () => {
+    let settings = getSettings();
+    window.api.send("saveSettings", settings);
+    $("#save-button").attr('style', 'color: green !important');
+    setTimeout(() => {
+        $("#save-button").attr("style", "");
+    }, 250);
 });
